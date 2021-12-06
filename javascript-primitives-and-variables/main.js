2257a65143a1e3ab51f8634d8349e9e778f1bbe3
Lyn Kim <lynkim216@outlook.com>
2021-12-06 10:59:00 -0800

    completed js primitives and variables exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Lyn Kim';
+var isCool = true;
+var totalPets = 1;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+var unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
