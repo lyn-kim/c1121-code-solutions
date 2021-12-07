dc8332eb07cec662158a578a67f0de0eaafdbec6
Lyn Kim <lynkim216@outlook.com>
2021-12-07 15:51:01 -0800

    completed js data model exercise

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,61 +1,76 @@
 var orderHistory = [
   {
     orderPlaced: 'August 4, 2020',
-    totalCost: '$34.00',
+    totalCost: 34.00,
     shipTo: 'JS Masher',
     orderNumber: '#114-3941689-8772232',
     orderDetails: true,
     viewInvoice: true,
     deliveryDate: 'Aug 8, 2020',
     deliveryResult: null,
-    productImg: 'img/js.jpg',
-    productName: 'JavaScript for Impatient programmers',
-    authorName: 'Rauschmayer, Dr.Axel',
-    returnWindowClose: 'Sep 7, 2020',
-    price: '$31.55',
-    buyAgain: true,
-    viewItem: true,
-    reviewProduct: true
+    itemList: [
+      {
+        type: 'book',
+        productImg: 'img/js.jpg',
+        productName: 'JavaScript for Impatient programmers',
+        authorName: 'Rauschmayer, Dr.Axel',
+        returnWindowClose: 'Sep 7, 2020',
+        price: 31.55,
+        buyAgain: true,
+        viewItem: true,
+        reviewProduct: true
+      }
+    ]
   },
   {
     orderPlaced: 'July 19, 2020',
-    totalCost: '$44.53',
+    totalCost: 44.53,
     shipTo: 'JS Masher',
     orderNumber: '#113-9984268-1280257',
     orderDetails: true,
     viewInvoice: true,
     deliveryDate: 'Jul 20,2020',
     deliveryResult: 'Your package was delivered. It was handed directly to a resident',
-    productImg: 'img/timeless.jpg',
-    productName: 'The Timeless Way of Building',
-    authorName: 'Alexander, Christopher',
-    returnWindowClose: 'Aug 19, 2020',
-    price: '$41.33',
-    buyAgain: true,
-    viewItem: true,
-    reviewProduct: true
+    itemList: [
+      {
+        type: 'book',
+        productImg: 'img/timeless.jpg',
+        productName: 'The Timeless Way of Building',
+        authorName: 'Alexander, Christopher',
+        returnWindowClose: 'Aug 19, 2020',
+        price: 41.33,
+        buyAgain: true,
+        viewItem: true,
+        reviewProduct: true
+      }
+    ]
   },
   {
     orderPlaced: 'July 4, 2020',
-    totalCost: '$17.22',
+    totalCost: 17.22,
     shipTo: 'JS Masher',
     orderNumber: '#114-2875557-9059409',
     orderDetails: true,
     viewInvoice: true,
     deliveryDate: 'Jul 7, 2020',
     deliveryResult: 'Your package was delivered. It was handed directly to a resident',
-    productImg: 'img/adapter.jpg',
-    productName: 'Gamecube Controller Adapter',
-    authorName: null,
-    returnWindowClose: 'Aug 5, 2020',
-    price: '$15.98',
-    buyAgain: true,
-    viewItem: true,
-    reviewProduct: true
+    itemList: [
+      {
+        type: 'hardware',
+        productImg: 'img/adapter.jpg',
+        productName: 'Gamecube Controller Adapter',
+        authorName: null,
+        returnWindowClose: 'Aug 5, 2020',
+        price: 15.98,
+        buyAgain: true,
+        viewItem: true,
+        reviewProduct: true
+      }
+    ]
   },
   {
     orderPlaced: 'July 3, 2020',
-    totalCost: '$138.93',
+    totalCost: 138.93,
     shipTo: 'JS Masher',
     orderNumber: '#113-2883177-2648248',
     orderDetails: true,
@@ -64,22 +79,24 @@ var orderHistory = [
     deliveryResult: null,
     itemList: [
       {
+        type: 'hardware',
         productImg: 'img/controller.jpg',
         productName: 'Game Cube Controller',
         authorName: null,
         returnWindowClose: 'Aug 4, 2020',
-        price: '$94.95',
+        price: 94.95,
         buyAgain: true,
         viewItem: true,
         getSupport: true,
         reviewProduct: true
       },
       {
+        type: 'book',
         productImg: 'img/art.jpg',
         productName: 'The Art of Sql',
         authorName: 'Faroult, Stephane',
         returnWindowClose: 'Aug 4, 2020',
-        price: '$33.99',
+        price: 33.99,
         buyAgain: true,
         viewItem: true,
         getSupport: true,
