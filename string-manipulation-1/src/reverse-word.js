50972902b21a21a1ad443e5a25dcf7d8533875d3
Lyn Kim <lynkim216@outlook.com>
2021-12-11 20:30:25 -0800

    completed string manipulation 1 exercise

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,8 +1,13 @@
 /* exported reverseWord */
 
-// create storage for string that has word in reverse order
-//
+// create storage for reverse-order string
+// take last letter of string and move to storage
+// give back reversed string
 
 function reverseWord(word) {
-
+  var reversed = '';
+  for (var i = (word.length - 1); i >= 0; i--) {
+    reversed += word[i];
+  }
+  return reversed;
 }
