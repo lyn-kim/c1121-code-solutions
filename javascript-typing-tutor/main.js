fe72b32aa7532ecf0fcb7b564a72edfb0f3e0a2d
Lyn Kim <lynkim216@outlook.com>
2021-12-16 21:56:14 -0800

    no changes

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,14 +1,13 @@
-var letter = document.querySelector('span');
+var letter = document.querySelectorAll('span');
 letter.addEventListener('keydown', checkLetter);
 
 function checkLetter(event) {
-
-  // for (var i = 0; i < letter.length; i++) {
-  //   if (event.key === letter[i]) {
-  //     letter[i].className = 'correct';
-  //   } else {
-  //     letter[i].className = 'incorrect';
-  //   }
-  // }
-
 }
+
+// for (var i = 0; i < letter.length; i++) {
+//   if (event.key === letter[i]) {
+//     letter[i].className = 'correct';
+//   } else {
+//     letter[i].className = 'incorrect';
+//   }
+// }
