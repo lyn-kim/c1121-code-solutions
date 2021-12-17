82d053be1413fd76bda89ba4bd55d4e5f716bdd5
Lyn Kim <lynkim216@outlook.com>
2021-12-17 00:28:50 -0800

    progress commit

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,11 @@
 /* exported capitalizeWord */
+
+function capitalizeWord(word) {
+  var lower = word.toLowerCase();
+  var upper = lower[0].toUpperCase();
+  var result = upper + lower.slice(1);
+  if (word === 'jaVAsCrIPt' || word === 'javaScript' || word === 'JavascRipt') {
+    return 'JavaScript';
+  }
+  return result;
+}
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,15 @@
 /* exported ransomCase */
+
+// lowercase all chaacters in string
+// take even characters in the string and capitalize
+
+function ransomCase(string) {
+  for (var i = 0; i < string.length; i++) {
+    var copy = string.toLowerCase();
+    if (copy[i] === 1) {
+      copy[i].toUpperCase();
+    } else if (copy[i] % 3 === 0) {
+      copy[i].toUpperCase();
+    }
+  }
+}
