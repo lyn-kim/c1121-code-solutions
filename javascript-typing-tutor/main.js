ab62bb004bef29b68bfd933139bfc70ea9286cf2
Lyn Kim <lynkim216@outlook.com>
2021-12-17 11:08:20 -0800

    changed event listener element to document

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,13 +1,12 @@
 var letter = document.querySelectorAll('span');
-letter.addEventListener('keydown', checkLetter);
+document.addEventListener('keydown', checkLetter);
 
 function checkLetter(event) {
+  for (var i = 0; i < letter.length; i++) {
+    if (event.target === letter[i]) {
+      letter[i].className = 'correct';
+    } else {
+      letter[i].className = 'incorrect';
+    }
+  }
 }
-
-// for (var i = 0; i < letter.length; i++) {
-//   if (event.key === letter[i]) {
-//     letter[i].className = 'correct';
-//   } else {
-//     letter[i].className = 'incorrect';
-//   }
-// }
