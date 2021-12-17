d2594fa5951c1f4e94c1c95875acbcbae762d950
Lyn Kim <lynkim216@outlook.com>
2021-12-16 21:29:13 -0800

    started typing tutor exercise

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,14 @@
+var letter = document.querySelector('span');
+letter.addEventListener('keydown', checkLetter);
+
+function checkLetter(event) {
+
+  // for (var i = 0; i < letter.length; i++) {
+  //   if (event.key === letter[i]) {
+  //     letter[i].className = 'correct';
+  //   } else {
+  //     letter[i].className = 'incorrect';
+  //   }
+  // }
+
+}
