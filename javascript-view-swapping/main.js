fb5314d898c2e8a4e04b8e27f3c984945c2f2ce0
Lyn Kim <lynkim216@outlook.com>
2021-12-16 17:43:39 -0800

    completed javascript view swapping exercise

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,35 @@
+var tabContainer = document.querySelector('.tab-container');
+var tab = document.querySelectorAll('.tab');
+var view = document.querySelectorAll('.view');
+
+tabContainer.addEventListener('click', tabChange);
+
+function tabChange(event) {
+
+  if (event.target.matches('.tab') !== true) {
+    return;
+  }
+
+  for (var i = 0; i < tab.length; i++) {
+    if (tab[i] === event.target) {
+      tab[i].className = 'tab active';
+    } else {
+      tab[i].className = 'tab';
+    }
+  }
+
+  var dataView = event.target.getAttribute('data-view');
+
+  if (event.target.matches('.tab') !== true) {
+    return;
+  }
+
+  for (i = 0; i < view.length; i++) {
+    if (view[i].getAttribute('data-view') === dataView) {
+      view[i].className = 'view';
+    } else {
+      view[i].className = 'view hidden';
+    }
+  }
+
+}
