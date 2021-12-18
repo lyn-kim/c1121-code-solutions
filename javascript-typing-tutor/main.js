7ec6bda55d739a3c5300256ff5f25fa2f56032b5
Lyn Kim <lynkim216@outlook.com>
2021-12-18 14:40:46 -0800

    event function for-loop basic structure

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,12 +1,35 @@
-var letter = document.querySelectorAll('span');
+
+var span = document.querySelectorAll('span');
 document.addEventListener('keydown', checkLetter);
 
 function checkLetter(event) {
-  for (var i = 0; i < letter.length; i++) {
-    if (event.target === letter[i]) {
-      letter[i].className = 'correct';
-    } else {
-      letter[i].className = 'incorrect';
+
+  for (var i = 0; i < span.length; i++) {
+    var currentLetter = span[i].innerText;
+    if (event.key === currentLetter) {
+      span[i].className = 'correct';
+      span[i + 1].className = 'next';
+      return;
+    } else if (event.key !== currentLetter) {
+      span[i].className = 'incorrect';
+      return;
     }
   }
+
 }
+
+// TEMP REUSE
+// if (event.key === currentLetter) {
+//   span[i].className = 'correct';
+// } else {
+//   span[i].className = 'next';
+// }
+
+// BOOLEAN VARIABLE OPTION
+// var correct = true;
+// if (correct) {
+//   for (var i = 0; i < span.length; i++) {
+//     span[i].className = 'correct';
+//     console.log('span[i]:', span[i]);
+//   }
+// }
