0ffeacd54fef81a05bbc3883b20fb72ec60af310
Lyn Kim <lynkim216@outlook.com>
2021-12-18 22:15:02 -0800

    no changes

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,16 +1,5 @@
 /* exported swapChars */
 
-// function swapChars(firstIndex, secondIndex, string) {
-//   var newString = string.split('');
-//   console.log(newString);
-//   var first = newString[firstIndex];
-//   var second = newString[secondIndex];
-
-//   newString = newString.replace(string[firstIndex], second);
-//   newString.replace(string[secondIndex], first);
-//   return newString.join('');
-// }
-
 function swapChars(firstIndex, secondIndex, string) {
   var newString = string;
   newString = newString.replace(string[firstIndex], string[secondIndex]);
