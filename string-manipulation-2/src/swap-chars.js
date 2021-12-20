da20fb93011411944257e92bc2775cdb50db7ae3
Lyn Kim <lynkim216@outlook.com>
2021-12-19 16:11:32 -0800

    string manipulation progress commit

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,8 +1,17 @@
 /* exported swapChars */
 
-function swapChars(firstIndex, secondIndex, string) {
-  var newString = string;
-  newString = newString.replace(string[firstIndex], string[secondIndex]);
-  newString = newString.replace(string[secondIndex], newString[firstIndex]);
-  return newString;
-}
+// function swapChars(firstIndex, secondIndex, string) {
+//   var newString = string.split('');
+//   var first = string[firstIndex];
+//   var second = string[secondIndex];
+
+//   console.log('string:', string);
+//   console.log('newString:', newString);
+//   console.log('firstindex letter:', first);
+//   console.log('secondindex letter:', second);
+
+//   newString.splice(firstIndex, 1);
+//   newString.splice(secondIndex, 0, first);
+//   console.log('newString:', newString);
+//   return newString.join('');
+// }
