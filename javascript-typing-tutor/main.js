0305c866ac5565b2377ce375467278d37e7c7118
Lyn Kim <lynkim216@outlook.com>
2021-12-24 08:42:27 -0800

    combine two same conditionals into one by using ||

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -5,18 +5,8 @@ document.addEventListener('keydown', checkLetter);
 function checkLetter(event) {
 
   for (var i = 0; i < span.length; i++) {
-    if (span[i].className === 'next') {
+    if (span[i].className === 'next' || span[i].className === 'incorrect') {
       var currentLetter = span[i].innerText;
-      if (event.key !== currentLetter) {
-        span[i].className = 'incorrect';
-      } else {
-        span[i].className = 'correct';
-        span[i + 1].className = 'next';
-      }
-      return;
-    }
-    if (span[i].className === 'incorrect') {
-      currentLetter = span[i].innerText;
       if (event.key !== currentLetter) {
         span[i].className = 'incorrect';
       } else {
