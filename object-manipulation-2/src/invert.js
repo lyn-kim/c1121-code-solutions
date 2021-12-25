801a277797f890b33666b8d4334cc5b400f3039e
Lyn Kim <lynkim216@outlook.com>
2021-12-24 16:24:10 -0800

    invert problem progress commit

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,38 @@
 /* exported invert */
+
+// put key[i] name to variable
+// put value of key[i] to variable
+// assign key[i] as a value of key[i]
+// assign value of key[i] as a key[i] name
+
+// var result = {};
+// function invert(source) {
+//   for (var i = 0; i < source.length; i++) {
+//     var keyName = source[i];
+//     console.log(keyName);
+//   }
+//   return result;
+// }
+
+// function getKeys(object) {
+//   var keyList = [];
+//   for (var key in object) {
+//     keyList.push(key);
+//   }
+//   return keyList;
+// }
+
+// function invert(source) {
+//   var result = {};
+//   for (var name in source) {
+//     var value = source[name];
+//     // console.log('source object value:', value);
+//     var keyname = name;
+//     // console.log('keyname:', keyname);
+//     // value = keyname;
+//     // console.log('value:', value);
+//     keyname = source[name];
+//     // console.log('keyname:', keyname);
+//   }
+//   return result;
+// }
