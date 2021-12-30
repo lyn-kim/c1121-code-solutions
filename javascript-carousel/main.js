ce56045ffda1dbb3edd713ad52443fed1ab16db8
Lyn Kim <lynkim216@outlook.com>
2021-12-30 00:07:35 -0800

    carousel progress commit

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,32 +1,58 @@
+
 var currentImage = document.querySelector('.pokemon');
-// var currentDot = document.querySelectorAll('.dot');
-// console.log('currentDot:', currentDot);
 var images = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
-var countIndex = 5;
-setInterval(switchImage, 1000);
-
 var imageIndex = 0;
-// var dotIndex = 0;
 
-function switchImage() {
+var dot = document.querySelectorAll('.dot');
+var dotIndex = 0;
+
+var countIndex = 5;
+
+var previous = document.querySelector('.previous');
+previous.addEventListener('click', goToPrevImage);
+function goToPrevImage(event) {
+  currentImage.src = images[imageIndex--];
+  if (imageIndex < 0) {
+    imageIndex = 4;
+  }
+}
+
+var next = document.querySelector('.next');
+next.addEventListener('click', goToNextImage);
+function goToNextImage(event) {
+  currentImage.src = images[imageIndex++];
+  if (imageIndex >= 5 || imageIndex <= 0) {
+    imageIndex = 0;
+  }
+}
+
+function showImage() {
   currentImage.src = images[imageIndex];
   imageIndex++;
   if (imageIndex === 5) {
     imageIndex = 0;
   }
 
-  // dotIndex++;
-  // if (dotIndex === 5) {
-  //   dotIndex = 0;
-  // }
-
-  // if (imageIndex === dotIndex) {
-  //   currentDot[dotIndex].className = 'dot active';
-  // } else {
-  //   currentDot[dotIndex] = 'dot';
-  // }
+  // for-loop for dots
+  dotIndex++;
+  for (var i = 0; i < dot.length; i++) {
+    if (i === dotIndex) {
+      dot[i].className = 'dot active';
+      dot[i - 1].className = 'dot';
+      if (dotIndex > dot.length) {
+        dotIndex = 0;
+      }
+    }
+  }
 
   clearInterval(countIndex);
 }
 
-// need loop for dots
+// function skipToImg(index) {
+//   dot[index].className = 'dot active';
+//   currentImage.src = images[index];
+//   dot[index].className = 'dot';
+// }
+
+setInterval(showImage, 3000);
+showImage(imageIndex);
