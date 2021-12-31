43e1934edd99c6aaf8a285b13be3a709997015ad
Lyn Kim <lynkim216@outlook.com>
2021-12-30 21:51:38 -0800

    completed carousel challenge

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,65 +1,39 @@
 
 var currentImage = document.querySelector('.pokemon');
 var images = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
+var dot = document.querySelectorAll('.dot');
 var imageIndex = 0;
 
-var dot = document.querySelectorAll('.dot');
-// var dotIndex = 0;
-
-var countIndex = 5;
-
-var previous = document.querySelector('.previous');
-previous.addEventListener('click', goToPrevImage);
-function goToPrevImage(event) {
-  currentImage.src = images[imageIndex--];
-  if (imageIndex < 0) {
-    imageIndex = 4;
-  }
-  // add increment/decrement here?
-}
-
-var next = document.querySelector('.next');
-next.addEventListener('click', goToNextImage);
-function goToNextImage(event) {
-  currentImage.src = images[imageIndex++];
-  if (imageIndex >= 5 || imageIndex <= 0) {
-    imageIndex = 0;
-  }
-  // add increment/decrement here?
-}
+setInterval(goForward, 3000);
 
 function showImage() {
-  // this line underneath should go somewhere below the codeblock
   currentImage.src = images[imageIndex];
-  imageIndex++;
-  if (imageIndex === 5) {
-    imageIndex = 0;
-  }
 
-  // for-loop for dots
-  for (var i = 0; i < dot.length; i++) {
+  for (var i = 0; i < images.length; i++) {
     if (i === imageIndex) {
       dot[i].className = 'dot fas fa-circle';
     } else {
       dot[i].className = 'dot far fa-circle';
     }
-    // if (dotIndex > dot.length) {
-    //   dotIndex = 0;
-    //   dotIndex++;
-    // }
   }
-
-  clearInterval(countIndex);
 }
 
-function skipToImg(event) {
-  var index = event.target.getAttribute('data-image');
-  dot[index].className = 'dot fas fa-circle';
-  currentImage.src = images[index];
+var next = document.querySelector('.next');
+next.addEventListener('click', goForward);
+function goForward() {
+  imageIndex++;
+  if (imageIndex >= 5) {
+    imageIndex = 0;
+  }
+  showImage();
 }
 
-var $dotContainer = document.querySelector('.dot-container');
-$dotContainer.addEventListener('click', skipToImg);
-
-setInterval(showImage, 1000);
-// showImage(imageIndex);
+var previous = document.querySelector('.previous');
+previous.addEventListener('click', goBackward);
+function goBackward() {
+  imageIndex--;
+  if (imageIndex < 0) {
+    imageIndex = 4;
+  }
+  showImage();
+}
