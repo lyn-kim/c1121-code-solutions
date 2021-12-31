ce3ea5e1c9489d90ed8961df3607ac661432b20b
Lyn Kim <lynkim216@outlook.com>
2021-12-30 23:14:20 -0800

    completed equal problem, unique problem in progress

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,5 +1,13 @@
 /* exported equal */
 
+// if length of first list is same as length of second list
+// for every item in the list, starting from the first item
+// check if item on the first list is same as item on the second list
+//    if not, return false
+// if length of first list is not same as length of second list
+//    return false
+// otherwise, return true
+
 function equal(first, second) {
   if (first.length === second.length) {
     for (var i = 0; i < first.length; i++) {
@@ -7,6 +15,8 @@ function equal(first, second) {
         return false;
       }
     }
+  } else {
+    return false;
   }
   return true;
 }
--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,15 @@
 /* exported unique */
+
+// function unique(array) {
+//   var result = [];
+//   for (var i = 0; i < array.length; i++) {
+//     if (array)
+//   }
+
+//     result.push(array[i]);
+//     if (array[i] !== result[i]) {
+//       return result;
+//     }
+//   }
+//   return result;
+// }
