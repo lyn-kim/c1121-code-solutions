36d8440a15f9b77a7c1902bb6a10469fea889312
Lyn Kim <lynkim216@outlook.com>
2022-01-12 17:55:54 -0800

    zip problem progress

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,29 @@
 /* exported zip */
+
+function zip(first, second) {
+  var newArray = [];
+  var finalArray = [];
+  for (var i = 0; i < first.length; i++) {
+    newArray.push(first[i]);
+    newArray.push(second[i]);
+  }
+
+  for (i = 0; i < newArray.length; i++) {
+    finalArray.push(newArray.slice(0, 1));
+    // console.log(finalArray);
+  }
+
+  return finalArray;
+}
+
+// function chunk(array, size) {
+//   var newArray = [];
+//   for (var i = 0; i < array.length; i += size) {
+//     if (i < array.length) {
+//       newArray.push(array.slice(i, size + i));
+//     } else {
+//       return newArray;
+//     }
+//   }
+//   return newArray;
+// }
