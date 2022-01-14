a062bb868e1dc00dfd2d579fdd542551c2099380
Lyn Kim <lynkim216@outlook.com>
2022-01-13 19:03:19 -0800

    completed unique problem

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,15 +1,18 @@
 /* exported unique */
 
-// function unique(array) {
-//   var result = [];
-//   for (var i = 0; i < array.length; i++) {
-//     if (array)
-//   }
+// assign an empty array to a results variable
+// for each string in the array
+// check if results contains current string
+// if no, move string to results array
+// if yes, skip
+// return result
 
-//     result.push(array[i]);
-//     if (array[i] !== result[i]) {
-//       return result;
-//     }
-//   }
-//   return result;
-// }
+function unique(array) {
+  var result = [];
+  for (var i = 0; i < array.length; i++) {
+    if (!(result.includes(array[i]))) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
