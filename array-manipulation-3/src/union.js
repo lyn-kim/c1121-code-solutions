4878e86b74cce5d2fc38e14a82ea1162a4fb6d25
Lyn Kim <lynkim216@outlook.com>
2022-01-13 20:23:11 -0800

    completed union problem

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,26 @@
 /* exported union */
+
+// create new variable and assign an empty array to store result
+// for each item in the first array
+// check if result array DOES NOT include current item
+// if yes, move current item to result array
+// for each item in the second array
+// check if result array DOES NOT include current item
+// if yes, move current item to result array
+// return result
+
+function union(first, second) {
+  var result = [];
+  for (var i = 0; i < first.length; i++) {
+    if (!result.includes(first[i])) {
+      result.push(first[i]);
+    }
+  }
+
+  for (i = 0; i < second.length; i++) {
+    if (!result.includes(second[i])) {
+      result.push(second[i]);
+    }
+  }
+  return result;
+}
