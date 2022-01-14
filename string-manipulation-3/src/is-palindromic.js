97f8a4c6891dcd6d7057825fa0a61de3e59a0ba4
Lyn Kim <lynkim216@outlook.com>
2022-01-14 15:52:35 -0800

    completed palindromic problem

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,14 +1,20 @@
 /* exported isPalindromic */
 
+// create storage for storing strings without spaces
+// create storage for storing half length of any string
 // for each character in the string
-// check to see if current character is equal to last character
+// check to see if the character at index mirroring the current character index is NOT equal to each other
+// if yes, return false
+// if no, return true
 
 function isPalindromic(string) {
+  var stringWithoutSpaces = string.replaceAll(' ', '');
+  var halfLength = Math.floor(string.length / 2);
 
-  for (var i = 0; i < string.length; i++) {
-    if (string[i] === string[string.length - 1 + i]) {
-      return true;
+  for (var i = 0; i < halfLength; i++) {
+    if (stringWithoutSpaces[i] !== stringWithoutSpaces[stringWithoutSpaces.length - i - 1]) {
+      return false;
     }
   }
-  return false;
+  return true;
 }
