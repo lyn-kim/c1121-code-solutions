86b281bc541b810eece5f3b6294e8bd7ff53715a
Lyn Kim <lynkim216@outlook.com>
2022-01-15 15:43:27 -0800

    completed reverse word problem

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -2,11 +2,8 @@
 
 // create variable to hold firstString with no spaces
 // create variable to hold secondString with no space
-// create variable to store length of first string with no spaces
-// create variable to store length of second string with no spaces
 // create variable to split, sort and join the charactets in first string
 // create variable to split, sort and join the characters in second string
-// if length of first string is equal to length of second string
 // check if the sorted version of the first string is equal to sorted version of second string
 // if yes, return true
 // if no, return false
@@ -14,16 +11,11 @@
 function isAnagram(firstString, secondString) {
   var firstStringNoSpaces = firstString.replaceAll(' ', '');
   var secondStringNoSpaces = secondString.replaceAll(' ', '');
-  var lengthFirst = firstStringNoSpaces.length;
-  var lengthSecond = secondStringNoSpaces.length;
   var sortedFirst = firstStringNoSpaces.split('').sort().join('');
   var sortedSecond = secondStringNoSpaces.split('').sort().join('');
 
-  if (lengthFirst === lengthSecond) {
-    if (sortedFirst === sortedSecond) {
-      return true;
-    }
+  if (sortedFirst === sortedSecond) {
+    return true;
   }
   return false;
-
 }
--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,24 @@
 /* exported reverseWords */
+
+// create storage for list of words from string
+// create storage for final string
+// for each word in the string
+//  split into individual letters and store in a list
+//  reverse the list and join into one string
+//  put the reversed string into final string list
+// join all the words together and store into result
+// give back result
+
+function reverseWords(string) {
+  var splitString = string.split(' ');
+  var finalString = [];
+
+  for (var i = 0; i < splitString.length; i++) {
+    var splitWord = splitString[i].split('');
+    var reverseWord = splitWord.reverse().join('');
+    finalString.push(reverseWord);
+  }
+  var result = finalString.join(' ');
+
+  return result;
+}
