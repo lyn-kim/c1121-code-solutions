c64d8d23eec0dd31dacf1ef6fdf6c72cec4fba32
Lyn Kim <lynkim216@outlook.com>
2022-01-15 19:57:52 -0800

    started deck problem

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,20 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+var players = [
+  {
+    player1: 'alice',
+    hand: null
+  },
+  {
+    player2: 'bob',
+    hand: null
+  },
+  {
+    player3: 'clark',
+    hand: null
+  },
+  {
+    player4: 'dan',
+    hand: null
+  }
+];
