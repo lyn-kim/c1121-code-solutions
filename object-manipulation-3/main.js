5e803beebb43f38ca066b301d1981fa7c4e93a7a
Lyn Kim <lynkim216@outlook.com>
2022-01-16 14:35:13 -0800

    card game in progress

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,20 +1,20 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
-var players = [
-  {
-    player1: 'alice',
-    hand: null
-  },
-  {
-    player2: 'bob',
-    hand: null
-  },
-  {
-    player3: 'clark',
-    hand: null
-  },
-  {
-    player4: 'dan',
-    hand: null
-  }
-];
+// var players = [
+//   {
+//     player1: 'alice',
+//     hand: null
+//   },
+//   {
+//     player2: 'bob',
+//     hand: null
+//   },
+//   {
+//     player3: 'clark',
+//     hand: null
+//   },
+//   {
+//     player4: 'dan',
+//     hand: null
+//   }
+// ];
