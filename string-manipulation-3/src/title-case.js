8f1b81d8a95f0082767925aface838567300d94d
Lyn Kim <lynkim216@outlook.com>
2022-01-17 19:07:53 -0800

    completed title case problem

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -12,17 +12,24 @@
 //    if yes, capitalize letter
 // check if current index is 0
 //    if yes, make first letter of word at current index uppercase
+// check if the word before current word is not undefined and it also ends with a colon
+//    if yes, uppercase the first letter of the current word and concantenate with rest of the words that are lowercased
+// check if current word includes a dash
+//    if yes, split the word where the dash occurs and store into storage
+//    create new storage to hold joined words
+//    for each split word,
+//        uppercase the first letter and concatenate with rest of the word that are lowercased
+//        put the word into storage
+//    join each capitalized word in storage with a dash
 // check if current word is Api or Javascript
 //    if yes, make it API or JavaScript
+// join all words and put into storage
+// return final string
 
 function titleCase(title) {
 
   var wordInTitle = title.split(' ');
   var capitalizedTitle = [];
-
-  // function capitalizeLowercaseWord(word) {
-  //   return word[0].toUpperCase() + word.slice(1);
-  // }
 
   for (var i = 0; i < wordInTitle.length; i++) {
     var lowercase = wordInTitle[i].toLowerCase();
@@ -61,15 +68,13 @@ function titleCase(title) {
 
     if (capitalizedTitle[i].includes('-')) {
       var splitCurrentWord = capitalizedTitle[i].split('-');
+      var joined = [];
 
-      for (i = 0; i < splitCurrentWord.length; i++) {
-        var capitalized = splitCurrentWord[i][0].toUpperCase() + splitCurrentWord[i].slice(1);
-        console.log('capitalized:', capitalized);
-        // console.log('splitCurrentWord:', splitCurrentWord);
-
+      for (var j = 0; j < splitCurrentWord.length; j++) {
+        var capitalized = splitCurrentWord[j][0].toUpperCase() + splitCurrentWord[j].slice(1);
+        joined.push(capitalized);
       }
-      // console.log('capitalizedTitle[i]:', capitalizedTitle[i]);
-      return capitalizedTitle[i];
+      capitalizedTitle[i] = joined.join('-');
     }
 
     if (capitalizedTitle[i] === 'Api') {
@@ -85,27 +90,3 @@ function titleCase(title) {
   var result = capitalizedTitle.join(' ');
   return result;
 }
-
-// if (capitalizedTitle[i].includes('-')) {
-//   var splitCurrentWord = capitalizedTitle[i].split('');
-//   if (splitCurrentWord[i - 1] === '-') {
-//     console.log('splitCurrentWord:', splitCurrentWord);
-//     var uppercase = splitCurrentWord[i].toUpperCase();
-//     console.log('uppercase:', uppercase);
-//     splitCurrentWord[i] = uppercase;
-//     console.log('splitCurrentWord[i]:', splitCurrentWord[i]);
-//   }
-//   capitalizedTitle[i] = splitCurrentWord.join('');
-// }
-
-// if (capitalizedTitle[i].includes('-')) {
-//   var splitCurrentWord = capitalizedTitle[i].split('');
-//   for (i = 0; i < splitCurrentWord.length; i++) {
-//     if (splitCurrentWord[i - 1] === '-') {
-//       var uppercase = splitCurrentWord[i].toUpperCase();
-//       splitCurrentWord[i] = uppercase;
-//     }
-//   }
-//   capitalizedTitle[i] = splitCurrentWord.join('');
-//   console.log('capitalizedTitle[i]:', capitalizedTitle[i]);
-// }
