3a76a01b3c8953a209f80078b8f2d9c287d35616
Lyn Kim <lynkim216@outlook.com>
2022-01-20 12:46:03 -0800

    completed writefile exercise

--- node-fs-writefile/note-to-self.js
+++ node-fs-writefile/note-to-self.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
+const note = '' + process.argv[2];
 
-fs.writeFile('note.txt', process.argv[2], err => {
+fs.writeFile('note.txt', note, 'utf8', err => {
   if (err) {
     console.error(err);
   }
