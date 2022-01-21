c7131cdd969f45fe50c70bbd7404aabebef5a5d8
Lyn Kim <lynkim216@outlook.com>
2022-01-21 13:09:02 -0800

    create a collection of objects to represent players and a collection to represent a 52 card deck

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,20 +1,39 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
-// var players = [
-//   {
-//     player1: 'alice',
-//     hand: null
-//   },
-//   {
-//     player2: 'bob',
-//     hand: null
-//   },
-//   {
-//     player3: 'clark',
-//     hand: null
-//   },
-//   {
-//     player4: 'dan',
-//     hand: null
-//   }
-// ];
+var players = [
+  {
+    player1: 'alice',
+    hand: null
+  },
+  {
+    player2: 'bob',
+    hand: null
+  },
+  {
+    player3: 'clark',
+    hand: null
+  },
+  {
+    player4: 'dan',
+    hand: null
+  }
+];
+
+const suit = ['club', 'diamond', 'heart', 'spade'];
+const rank = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king'];
+
+const deck = [];
+function createDeck() {
+  for (var i = 0; i < suit.length; i++) {
+    for (var j = 0; j < rank.length; j++) {
+      const card = {
+        suit: suit[i],
+        rank: rank[j]
+      };
+      deck.push(card);
+    }
+  }
+  return deck;
+}
+
+createDeck();
