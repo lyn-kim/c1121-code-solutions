b69e153ae7324c451dde890f384ec8962e2810b6
Lyn Kim <lynkim216@outlook.com>
2022-01-23 19:53:23 -0800

    completed object manipulation 3

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -2,19 +2,19 @@ console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
 var players = [
   {
-    player1: 'alice',
+    name: 'alice',
     hand: null
   },
   {
-    player2: 'bob',
+    name: 'bob',
     hand: null
   },
   {
-    player3: 'clark',
+    name: 'clark',
     hand: null
   },
   {
-    player4: 'dan',
+    name: 'dan',
     hand: null
   }
 ];
@@ -48,4 +48,47 @@ function shuffle() {
 }
 
 shuffle();
-console.log('shuffled deck:', shuffledDeck);
+
+function dealCards() {
+  players[0].hand = [shuffledDeck.pop(), shuffledDeck.pop()];
+  players[1].hand = [shuffledDeck.pop(), shuffledDeck.pop()];
+  players[2].hand = [shuffledDeck.pop(), shuffledDeck.pop()];
+  players[3].hand = [shuffledDeck.pop(), shuffledDeck.pop()];
+}
+
+dealCards();
+
+function changeToNumber() {
+  for (let i = 0; i < players.length; i++) {
+    for (let j = 0; j < players[i].hand.length; j++) {
+      if (players[i].hand[j].rank === 'ace') {
+        players[i].hand[j].rank = 11;
+      } else if (players[i].hand[j].rank === 'jack' || players[i].hand[j].rank === 'queen' || players[i].hand[j].rank === 'king') {
+        players[i].hand[j].rank = 10;
+      }
+    }
+  }
+  return players;
+}
+changeToNumber();
+
+function addScore() {
+  for (let i = 0; i < players.length; i++) {
+    players[i].finalScore = players[i].hand[0].rank + players[i].hand[1].rank;
+  }
+}
+addScore();
+
+players.sort(function (a, b) {
+  return a.finalScore - b.finalScore;
+});
+
+function winner() {
+  if (players[2].finalScore === players[3].finalScore) {
+    console.log(`${players[2].name} and ${players[3].name} tied!`);
+  } else {
+    console.log(`${players[3].name} won!`);
+  }
+}
+
+winner();
