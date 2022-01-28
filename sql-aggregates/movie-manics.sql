d22ab1c6e456f60bb711d90f589b6028f476b6e2
Lyn Kim <lynkim216@outlook.com>
2022-01-27 19:16:12 -0800

    completed sql-aggregates exercise

--- sql-aggregates/movie-manics.sql
+++ sql-aggregates/movie-manics.sql
@@ -1,5 +1,7 @@
 select "customers"."firstName",
        "customers"."lastName",
-       "payments"."customerId"
+       sum("payments"."amount") as "totalSpent"
   from "customers"
   join "payments" using ("customerId")
+  group by "customers"."customerId"
+order by "totalSpent" desc;
