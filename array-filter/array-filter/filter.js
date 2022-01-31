6fddac5a84312cf511acea50b17324906a118c8a
Lyn Kim <lynkim216@outlook.com>
2022-01-31 11:20:53 -0800

    filter method the OG style

--- array-filter/array-filter/filter.js
+++ array-filter/array-filter/filter.js
@@ -1,3 +1,9 @@
 function filter(array, predicate) {
-
+  const outputArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (predicate(array[i])) {
+      outputArray.push(array[i]);
+    }
+  }
+  return outputArray;
 }
