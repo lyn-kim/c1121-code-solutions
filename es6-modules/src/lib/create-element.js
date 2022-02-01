7afc14395405d4bf491a8105b15376c3105a68b4
Lyn Kim <lynkim216@outlook.com>
2022-02-01 13:01:00 -0800

    completed es6-modules exercise

---
+++ es6-modules/src/lib/create-element.js
@@ -0,0 +1,20 @@
+import toArray from './to-array';
+
+// const toArray = require('./to-array');
+
+function createElement(tagName, attributes, children = []) {
+  const parent = document.createElement(tagName);
+  for (const name in attributes) {
+    parent.setAttribute(name, attributes[name]);
+  }
+  toArray(children).forEach(child => {
+    if (!(child instanceof HTMLElement)) {
+      child = document.createTextNode(child);
+    }
+    parent.appendChild(child);
+  });
+  return parent;
+}
+
+export default createElement;
+// module.exports = createElement;
