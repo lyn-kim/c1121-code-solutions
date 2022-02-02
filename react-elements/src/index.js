9cb14224eb59a45cc22e07ab52d6ef8fa0c6e4b8
Lyn Kim <lynkim216@outlook.com>
2022-02-01 16:51:15 -0800

    exercise review

--- react-elements/src/index.js
+++ react-elements/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 
 const text = React.createElement(
   'h1',
-  [null],
-  ['Hello, React!']
+  null,
+  'Hello, React!'
 );
 
 ReactDOM.render(
   text,
   document.getElementById('root')
 );
+
+// const element = React.createElement('h1', null, 'Hello, React');
+// const container = document.getElementById('root');
+// ReactDOM.render(
+//   element,
+//   container
+//   );
