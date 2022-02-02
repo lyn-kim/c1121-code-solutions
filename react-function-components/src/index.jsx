a58f568a8b8db283ebf577be0b247936127a958b
Lyn Kim <lynkim216@outlook.com>
2022-02-02 11:58:40 -0800

    no changes

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function CustomButton() {
-  return <button>Click, Me!</button>;
-}
-
 // function CustomButton() {
-//   return (
-//     <button>Click Me!</button>
-//   );
+//   return <button>Click, Me!</button>;
 // }
+
+function CustomButton() {
+  return (
+    <button>Click Me!</button>
+  );
+}
 
 const buttonContainer = document.querySelector('div#root');
 
-ReactDOM.render(CustomButton(), buttonContainer);
+// ReactDOM.render(CustomButton(), buttonContainer);
 
-// ReactDOM.render(
-//   <CustomButton />,
-//   buttonContainer
-// );
+ReactDOM.render(
+  <CustomButton />,
+  buttonContainer
+);
