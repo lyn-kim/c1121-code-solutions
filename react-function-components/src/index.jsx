1ec8562a04322d7879db2b11562a70dc9d459f9b
Lyn Kim <lynkim216@outlook.com>
2022-02-02 11:53:14 -0800

    alternative ways to write and render CustomButton

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -5,6 +5,17 @@ function CustomButton() {
   return <button>Click, Me!</button>;
 }
 
+// function CustomButton() {
+//   return (
+//     <button>Click Me!</button>
+//   );
+// }
+
 const buttonContainer = document.querySelector('div#root');
 
 ReactDOM.render(CustomButton(), buttonContainer);
+
+// ReactDOM.render(
+//   <CustomButton />,
+//   buttonContainer
+// );
