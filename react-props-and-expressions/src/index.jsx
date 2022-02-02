85eff12a2f5a5a421fbb9c50a0da4d824e383c4b
Lyn Kim <lynkim216@outlook.com>
2022-02-02 13:18:34 -0800

    added notes for code-reading

--- react-props-and-expressions/src/index.jsx
+++ react-props-and-expressions/src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+// importing react from react package
 import ReactDOM from 'react-dom';
 
+// line 7 - returning the result of an expression of the react element button
+// with a js expression props.text
 function CustomButton(props) {
   return (
     <button> {props.text} </button>
@@ -9,6 +12,10 @@ function CustomButton(props) {
 
 const buttonContainer = document.querySelector('#root');
 
+// assigning the result of an expression to the const variable element
+// opening div for the React element
+// React element CustomButton with a
+// prop text with a value (js expression, number, etc)
 const element = (
   <div>
     <CustomButton text="I" />
