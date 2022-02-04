0ecc2e21aa71da743f5f51d2bc2df1eb27f7a897
Lyn Kim <lynkim216@outlook.com>
2022-02-03 17:13:43 -0800

    completed react-rendering-lists exercise

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+function PokemonList(props) {
+  const pokedex = props.pokedex;
+  const listItems = pokedex.map(pokemon =>
+    <li key={pokemon.toString()}>
+      {pokemon.name}
+    </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+ReactDOM.render(
+  <PokemonList pokedex={pokedex} />,
+  document.getElementById('root')
+);
