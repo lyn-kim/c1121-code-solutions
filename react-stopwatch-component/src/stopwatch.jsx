61d11b92ad15e9793f30bcd9734da226ccd6f140
Lyn Kim <lynkim216@outlook.com>
2022-02-03 22:58:05 -0800

    completed begin and pause functions

---
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+export default class Stopwatch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      ticking: false
+    };
+    this.startTimer = this.startTimer.bind(this);
+    this.pauseTimer = this.pauseTimer.bind(this);
+    this.begin = this.begin.bind(this);
+    this.pause = this.pause.bind(this);
+  }
+
+  startTimer() {
+    this.intervalId = setInterval(() => {
+      this.setState(state => {
+        return {
+          count: state.count + 1
+        };
+      });
+    }, 1000);
+  }
+
+  pauseTimer() {
+    clearInterval(this.intervalId);
+  }
+
+  begin() {
+    this.setState({
+      ticking: true
+    });
+    this.startTimer();
+  }
+
+  pause() {
+    this.setState({
+      ticking: false
+    });
+    this.pauseTimer();
+  }
+
+  render() {
+    const count = this.state.count;
+    const status = this.state.ticking;
+    if (!status) {
+      return <div className="container">
+        <div className="face">
+          <h1 className="time">{count}</h1>
+        </div>
+        <div onClick={this.begin} className="action"><i className="fas fa-play fa-2x"></i></div>
+      </div>;
+    }
+    return <div className="container">
+      <div className="face">
+        <h1 className="time">{count}</h1>
+      </div>
+      <div onClick={this.pause} className="action"><i className="fas fa-pause fa-2x"></i></div>
+    </div>;
+  }
+}
