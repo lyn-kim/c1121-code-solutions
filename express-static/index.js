17fab2d989cb4d2b44e330c5a6c3ae6f38e620b4
Lyn Kim <lynkim216@outlook.com>
2022-02-07 10:58:52 -0800

    call path.join with two arguments, call express.static with const directory

--- express-static/index.js
+++ express-static/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const directory = path.join('__dirname/public');
-const access = express.static('public');
+const directory = path.join(__dirname, 'public');
+const access = express.static(directory);
 
 app.use(access);
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
-  console.log('directory:', directory);
+  // console.log('directory:', directory);
   console.log('Express server listening on port 3000');
 });
