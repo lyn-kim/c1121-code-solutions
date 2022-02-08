209e718a14b2dd345b417a97bbb90def68704315
Lyn Kim <lynkim216@outlook.com>
2022-02-08 12:42:27 -0800

    pass topics array to component via props in index.jsx

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,27 +1,9 @@
 import React from 'react';
-
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeTopicId: null,
-      topics: [
-        {
-          id: 1,
-          name: 'Hypertext Markup Language',
-          detail: 'HTML(HyperText Markup Language) is the most basic building block of the Web.It defines the meaning and structure of web content.'
-        },
-        {
-          id: 2,
-          name: 'Cascading Style Sheets',
-          detail: 'Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML. CSS describes how elements should be rendered on screen, on paper, in speech, or on other media.'
-        },
-        {
-          id: 3,
-          name: 'JavaScript',
-          detail: 'JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles.'
-        }
-      ]
+      activeTopicId: null
     };
     this.toggleTopic = this.toggleTopic.bind(this);
   }
@@ -35,9 +17,10 @@ export default class Accordion extends React.Component {
   }
 
   render() {
+    // console.log(this.props.topics);
     return (
       <div className="container">
-        {this.state.topics.map(topic => {
+        {this.props.topics.map(topic => {
           return (
             <React.Fragment key={topic.id}>
               <div onClick={() => this.toggleTopic(topic.id)} className="topic">
