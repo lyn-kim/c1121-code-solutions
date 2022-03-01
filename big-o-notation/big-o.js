20a34e7d197e55836f44abaa9fd24e826542b4e9
Lyn Kim <lynkim216@outlook.com>
2022-02-28 16:00:55 -0800

    check for linear time inside loops

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -25,15 +25,15 @@ function uniqueQuadratic(words) {
     i++                             // 2 * n = O(2n) ~= O(n)
   ) {
     const word = words[i];          // 2 * n = O(2n) ~= O(n)
-    let isUnique = true;            // 1 * 1 = O(1)
+    let isUnique = true;            // 1 * n = O(n)
     for (
-      let c = 0;                    // 1 * 1 = O(1)
+      let c = 0;                    // 1 * n = O(n)
       c < i;                        // 2 * n^2 = O(2n^2)
       c++                           // 2 * n^2 = O(2n^2) ~= O(n^2)
     ) {
       const comparing = words[c];   // 2 * n^2 = O(2n^2) ~= O(n^2)
       if (comparing === word) {     // 1 * n^2 = O(n^2)
-        isUnique = false;           // 1 * 1 = O(1)
+        isUnique = false;           // 1 * n = O(n)
       }
     }
     if (isUnique) {                 // 1 * 1 = O(1)
