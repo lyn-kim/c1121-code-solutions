c8cef5b99350e13863302e8f4a2aa94a03dcc2bc
Lyn Kim <lynkim216@outlook.com>
2022-03-01 15:39:24 -0800

    balanced braces note

---
+++ data-structures-stacks/note.js
@@ -0,0 +1,8 @@
+// balanced braces
+
+// define a function that takes a string including parentheses, brackets, braces
+// return true/false if the braces are balanced
+// '{({[]{([])}})}' --> true
+// '{({[]{([)]}})}' -- > false
+
+// browser history is a stack
